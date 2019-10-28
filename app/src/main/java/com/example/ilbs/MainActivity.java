535fa9a1dad11347b6789f72d1318be9f5bc33cd
GUHEHET <wodnr96@gmail.com>
2019-10-28 23:01:51 +0900

    Run with Thread

--- app/src/main/java/com/example/ilbs/MainActivity.java
+++ app/src/main/java/com/example/ilbs/MainActivity.java
@@ -5,43 +5,42 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    private java.net.Socket socket; //연결 소켓
-    private BufferedReader in; //읽어오는 버퍼
-    private BufferedWriter out; //쓰는 버퍼
-    private String ip = "127.0.0.1"; //연결 IP
-    private int port = 9999; //연결 포트
+import static java.lang.Thread.sleep;
 
-    //와이파이 받아오기용 변수
+public class MainActivity extends AppCompatActivity implements Runnable {
     private static int REQUEST_ACCESS_FINE_LOCATION = 1000;
-    private WifiManager wifiMan;
-    private List<ScanResult> results;
+    private WifiManager wifiMan; //와이파이 매니저
+    private List<ScanResult> results; //와이파이 리스트
+    private boolean done = false; //스레드 종료 조건
+
+    Thread sThread = new Thread(MainActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //인터넷 권한 확인
             int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
 
             if (permissionCheck == PackageManager.PERMISSION_DENIED) {
@@ -49,61 +48,68 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        wifiMan = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
-        Thread socketWorker = new Thread() {
-            public void run() {
-                try {
-                    socket = new Socket(ip, port);
-                    out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                } catch (IOException e) {
-                    Log.i("Socket Connect", "Connect Error.");
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        socketWorker.start();
-    }
-
-    private BroadcastReceiver Receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            results = wifiMan.getScanResults();
-
-            Log.i("List Test", String.format("%d network found", results.size()));
-            for (ScanResult result : results) {
-
-                Log.i("List Test", String.format("SSID : %s, BSSID : %s, Level : %d, Freq : %d", result.SSID, result.BSSID, result.level, result.frequency));
-
-                try {
-                    out.write(String.format("SSID : %s, BSSID : %s, Level : %d, Freq : %d", result.SSID, result.BSSID, result.level, result.frequency));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    };
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_TIME_TICK);
-        this.registerReceiver(Receiver, intentFilter);
-        wifiMan.startScan();
+        sThread.setDaemon(true);
+        sThread.start(); //thread 생성
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() { //종료시 스레드 종료
         super.onDestroy();
-        unregisterReceiver(Receiver);
+        done = true;
+        sThread.interrupt();
+    }
+
+    @Override
+    public void run() {
+        Socket socket = null; //연결 소켓
+        OutputStream out = null; //쓰는 버퍼
+        InputStream in = null; //읽어오는 버퍼
+        String ip = "172.30.1.30"; //연결 IP
+        int port = 9999; //연결 포트
+
+        try { //소켓 연결
+            socket = new Socket(ip, port);
+            out = socket.getOutputStream();
+            in = socket.getInputStream();
+        } catch (IOException e) {
+            Log.i("Socket Connect", "Error.");
+            e.printStackTrace();
+        }
+
+        wifiMan = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE); //와이파이 정보 받아오기
 
         try {
-            socket.close();
+            while(!done) { //어플리케이션 종료시까지
+                results = wifiMan.getScanResults();
+
+                Log.i("List Test", String.format("%d network found", results.size()));
+
+                for (ScanResult result : results) { //와이파이 정보 전송
+                    String txt = String.format("SSID : %s, BSSID : %s, Level : %d, Freq : %d*", result.SSID, result.BSSID, result.level, result.frequency);
+                    Log.i("List Test", txt);
+                    byte[] outText  = txt.getBytes();
+                    out.write(outText);
+                    out.flush();
+                }
+                try {
+                    sleep(1000 * 10); //n초만큼 대기 (현재 10초 -> 1~5분 예정)
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try {
+            socket.close(); //소켓 닫기
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.i("Socket Test", "Socket Closed.");
+    }
+
+    public void onReset(View view) { //리셋버튼 눌렀을 시 interrupt 발생, 다시 정보 발송
+        sThread.interrupt();
     }
 }
