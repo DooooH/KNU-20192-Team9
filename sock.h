bfdb9e3459b764660febca464385c4ad5849e6e2
latter2005 <45479174+latter2005@users.noreply.github.com>
2019-11-01 19:06:39 +0900

    gui 맵 제외
    버그때문에 맵처리 코드는 빼고
    리스트 더블클릭 이벤트로 수정 삭제
    db코드 범용적으로 쓸 수 있게 update 수정

---
+++ sock.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+#include <WinSock2.h>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#pragma comment (lib , "ws2_32.lib")
+#define MAX_BUF_SIZE 2048
+
+class SERVER {
+private:
+	std::string MAC;
+	std::string IP;
+	SOCKET sock;
+	sockaddr_in addr;
+public:
+
+	SERVER();
+	//SERVER() = delete;
+	SERVER(ULONG IP, u_short port);
+	//~SERVER();
+	void close_server();
+	/*
+	bool set_sockaddr();*/
+	SOCKET server_accept(sockaddr_in* clnt_addr, int* addrlen);
+
+};
+
+class CLIENT {
+private:
+	std::string MAC;
+	ULONG IP;
+	SOCKET sock;
+	sockaddr_in addr;
+	//std::thread::id thread_id;
+
+public:
+	CLIENT();
+	CLIENT(SOCKET sock, sockaddr_in addr);
+	//CLIENT(SOCKET sock, sockaddr_in addr, std::thread::id thread_id);
+	~CLIENT();
+	void close_sock();
+	bool operator==(CLIENT other);
+	size_t send_msg(char *msg);
+	int recv_msg(char* buf);
+
+	/*
+	void _handle();*/
+};
+
+void _handle(CLIENT clnt);
+int android_server();
+
+
+extern SERVER *server;
+extern std::vector <CLIENT> client_list;
+extern std::mutex client_list_mtx;
